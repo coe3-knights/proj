8de0e1f4a7b3ee89d00f1e4a6f35abead18e20f7
cbenisson <35075194+cbenisson@users.noreply.github.com>
2019-06-10 18:55:00 +0000

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    PROJECTS_PER_PAGE = 10
+    #PROJECTS_PER_PAGE = 10
     WHOOSH_BASE = os.path.join(basedir, 'whoosh_base')
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
+    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
     MAIL_USERNAME = os.environ.get('MAIL_USER')
     MAIL_PASSWORD = os.environ.get('MAIL_PSWD')
     ADMINS = ['knights.of.coe3@gmail.com']
