c37b9c5fb48be693f2e20800d3321c4107fdbe74
Godwin Krieger <32751086+GodwinKrieger@users.noreply.github.com>
2019-06-12 00:14:16 +0100

    Update projects.py

--- app/api/projects.py
+++ app/api/projects.py
@@ -77,7 +77,7 @@ def upload():
             new_project.tags = request.form.get('tags')
             new_project.date_created = request.form.get('date_created')
             new_project.hashFilename(filename)
-            new_project.author = g.current_user
+            #new_project.author = g.current_user
         
             try:
                new_project.file_data = file.read()
